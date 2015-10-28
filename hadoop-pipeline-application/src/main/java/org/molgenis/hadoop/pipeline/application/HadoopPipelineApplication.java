6d05d40948375507c5e306263992e77ef5a27321
Sander van den Hoek <savandenhoek@gmail.com>
2015-10-28 13:01:10 +0100

    Hadoop Generic Options are now supported. Furthermore, the HadoopPipelineApplication now writes to logger as well.

--- hadoop-pipeline-application/src/main/java/org/molgenis/hadoop/pipeline/application/HadoopPipelineApplication.java
+++ hadoop-pipeline-application/src/main/java/org/molgenis/hadoop/pipeline/application/HadoopPipelineApplication.java
@@ -3,7 +3,6 @@ package org.molgenis.hadoop.pipeline.application;
 import java.io.IOException;
 
 import org.apache.commons.cli.ParseException;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.NullWritable;
@@ -11,8 +10,10 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
 import org.molgenis.hadoop.pipeline.application.inputdigestion.CommandLineInputParser;
 import org.molgenis.hadoop.pipeline.application.mapreduce.HadoopPipelineMapper;
 
@@ -24,6 +25,11 @@ import mr.wholeFile.WholeFileInputFormat;
 public class HadoopPipelineApplication extends Configured implements Tool
 {
 	/**
+	 * Logger to write information to.
+	 */
+	private static final Logger logger = Logger.getLogger(HadoopPipelineApplication.class);
+
+	/**
 	 * Default main class that calls {@link ToolRunner} to execute Hadoop MapReduce.
 	 * 
 	 * @param args
@@ -32,15 +38,25 @@ public class HadoopPipelineApplication extends Configured implements Tool
 	{
 		try
 		{
-			ToolRunner.run(new Configuration(), new HadoopPipelineApplication(), args);
+			// Digests Hadoop's Generic Options.
+			GenericOptionsParser genericOptionsParser = new GenericOptionsParser(args);
+
+			// Run the Hadoop application.
+			try
+			{
+				ToolRunner.run(genericOptionsParser.getConfiguration(), new HadoopPipelineApplication(),
+						genericOptionsParser.getRemainingArgs());
+			}
+			catch (Exception e) // Catches all internal errors.
+			{
+				logger.error(e.getMessage());
+			}
 		}
-		catch (Exception ex)
+		catch (IOException e) // Catches errors caused by Hadoop's GenericOptionsParser.
 		{
-			System.err.println("An error occured!");
-			System.err.println(ex.getMessage());
-			System.err.println(""); // Empty line before stack trace.
-			ex.printStackTrace();
+			logger.error(e.getMessage());
 		}
+
 	}
 
 	/**
@@ -55,21 +71,20 @@ public class HadoopPipelineApplication extends Configured implements Tool
 	 */
 	public int run(String[] args) throws IOException, ParseException, ClassNotFoundException, InterruptedException
 	{
-		Configuration conf = getConf();
+		// Writes Configuration properties to logger.debug that can (and have) cause(d) out of memory/timeout errors.
+		logger.debug("mapreduce.map.memory.mb: " + getConf().get("mapreduce.map.memory.mb"));
+		logger.debug("mapreduce.map.java.opts: " + getConf().get("mapreduce.map.java.opts"));
+		logger.debug("mapreduce.task.timeout: " + getConf().get("mapreduce.task.timeout"));
 
-		// YARN fix for: java.lang.OutOfMemoryError: Java heap space
-		conf.set("mapreduce.map.memory.mb", "2048");
-		conf.set("mapreduce.map.java.opts", "-Xmx1024m");
+		FileSystem fileSys = FileSystem.get(getConf());
 
-		FileSystem fileSys = FileSystem.get(conf);
-
-		// Digests command line arguments.
+		// Digests application-specific command line arguments.
 		CommandLineInputParser parser = new CommandLineInputParser(fileSys, args);
 
 		// Only execute MapReduce job if all required paremeters are present and correct.
 		if (parser.isContinueApplication())
 		{
-			Job job = Job.getInstance(conf);
+			Job job = Job.getInstance(getConf());
 			job.setJarByClass(HadoopPipelineApplication.class);
 			job.setJobName("HadoopPipelineApplication");
 
@@ -98,10 +113,12 @@ public class HadoopPipelineApplication extends Configured implements Tool
 			job.setOutputKeyClass(NullWritable.class);
 			job.setOutputValueClass(Text.class);
 
-			job.waitForCompletion(true);
-
+			// Returns 0 if job completed successfully. If not, returns 1.
+			return job.waitForCompletion(true) ? 0 : 1;
 		}
-		return 0;
+
+		// Returns exit code 1 if no job was executed.
+		return 1;
 	}
 
 }
