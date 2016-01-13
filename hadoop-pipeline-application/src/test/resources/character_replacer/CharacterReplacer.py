739c1a26d99474f408f54ae1df1424378d66973f
Sander van den Hoek <savandenhoek@gmail.com>
2016-01-13 13:26:02 +0100

    Fixed exclusion of a Python test script from the git repo.

---
+++ hadoop-pipeline-application/src/test/resources/character_replacer/CharacterReplacer.py
@@ -0,0 +1,18 @@
+#!/user/bin/env python3
+
+import sys
+import fileinput
+
+
+def main():
+    """
+    Simple script for the Java TestNG tests. Expects a String as input from stdin and converts a given character to a
+    different character. The line is then written to stdout.
+    """
+    if len(sys.argv) != 3:
+        print("invalid number of arguments. Requires 2: CharacterReplacer.py <character to replace> <character to replace with>")
+    else:
+        for line in sys.stdin:
+            sys.stdout.write(str(line).replace(sys.argv[1], sys.argv[2]))
+
+main()
