3d0aaed06fecfd49e54a0a630cd02316615a66e3
Sander van den Hoek <savandenhoek@gmail.com>
2016-02-15 13:59:08 +0100

    Made the creation of the SamFileHeader more generic.

---
+++ hadoop-pipeline-application/src/main/java/org/molgenis/hadoop/pipeline/application/cachedigestion/SamFileHeaderGenerator.java
@@ -0,0 +1,56 @@
+package org.molgenis.hadoop.pipeline.application.cachedigestion;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.molgenis.hadoop.pipeline.application.DistributedCacheHandler;
+
+import htsjdk.samtools.SAMFileHeader;
+import htsjdk.samtools.SAMProgramRecord;
+import htsjdk.samtools.SAMSequenceDictionary;
+
+public abstract class SamFileHeaderGenerator
+{
+	/**
+	 * Retrieves the {@link SAMFileHeader} using the cache files. When adjusting the Mapper/Reducer, a manual validation
+	 * of this method is required to see if it is still up-to-date!!!
+	 * 
+	 * @param context
+	 *            {@link TaskAttemptContext}
+	 * @return {@link SAMFileHeader}
+	 * @throws IllegalArgumentException
+	 * @throws IOException
+	 */
+	public static SAMFileHeader retrieveSamFileHeader(TaskAttemptContext context)
+			throws IllegalArgumentException, IOException
+	{
+		DistributedCacheHandler cacheHandler = new DistributedCacheHandler(context);
+
+		SAMFileHeader samFileHeader = new SAMFileHeader();
+
+		// Adds @SQ tags data to the SAMFileHeader.
+		String alignmentReferenceDictFile = cacheHandler.getReferenceDictFile();
+		SAMSequenceDictionary seqDict = new HadoopRefSeqDictReader().read(alignmentReferenceDictFile);
+		samFileHeader.setSequenceDictionary(seqDict);
+
+		// Retrieves the tools data stored in the tools archive info.xml file.
+		String toolsArchiveInfoXml = cacheHandler.getInfoXmlFileFromToolsArchive();
+		Map<String, SAMProgramRecord> tools = new HadoopToolsXmlReader().read(toolsArchiveInfoXml);
+
+		// Add the @PG tags for the tools within the tools archive that were used (define manually!!!).
+		samFileHeader.addProgramRecord(tools.get("bwa"));
+
+		// Retrieves the samples stored in the samples information file and adds them as SAMReadGroupRecords (@RG tags).
+		String samplesInfoFile = cacheHandler.getSamplesInfoFile();
+		List<Sample> samples = new HadoopSamplesInfoFileReader().read(samplesInfoFile);
+		for (Sample sample : samples)
+		{
+			samFileHeader.addReadGroup(sample.getAsReadGroupRecord());
+		}
+
+		// Returns the completed SAMFileHeader.
+		return samFileHeader;
+	}
+}
--- hadoop-pipeline-application/src/main/java/org/molgenis/hadoop/pipeline/application/formats/BamOutputFormat.java
+++ hadoop-pipeline-application/src/main/java/org/molgenis/hadoop/pipeline/application/formats/BamOutputFormat.java
@@ -1,26 +1,17 @@
 package org.molgenis.hadoop.pipeline.application.formats;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 import org.apache.hadoop.mapreduce.OutputFormat;
 import org.apache.hadoop.mapreduce.RecordWriter;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.molgenis.hadoop.pipeline.application.DistributedCacheHandler;
-import org.molgenis.hadoop.pipeline.application.HadoopPipelineApplication;
-import org.molgenis.hadoop.pipeline.application.cachedigestion.HadoopRefSeqDictReader;
-import org.molgenis.hadoop.pipeline.application.cachedigestion.HadoopSamplesInfoFileReader;
-import org.molgenis.hadoop.pipeline.application.cachedigestion.HadoopToolsXmlReader;
-import org.molgenis.hadoop.pipeline.application.cachedigestion.Sample;
+import org.molgenis.hadoop.pipeline.application.cachedigestion.SamFileHeaderGenerator;
 import org.seqdoop.hadoop_bam.BAMOutputFormat;
 import org.seqdoop.hadoop_bam.KeyIgnoringBAMOutputFormat;
 import org.seqdoop.hadoop_bam.KeyIgnoringBAMRecordWriter;
 import org.seqdoop.hadoop_bam.SAMRecordWritable;
 
 import htsjdk.samtools.SAMFileHeader;
-import htsjdk.samtools.SAMProgramRecord;
-import htsjdk.samtools.SAMSequenceDictionary;
 
 /**
  * Custom implementation replacing {@link KeyIgnoringBAMOutputFormat} where the {@link SAMFileHeader} is generated using
@@ -35,48 +26,7 @@ public class BamOutputFormat<K> extends BAMOutputFormat<K>
 	@Override
 	public RecordWriter<K, SAMRecordWritable> getRecordWriter(TaskAttemptContext ctx) throws IOException
 	{
-		return new KeyIgnoringBAMRecordWriter<K>(getDefaultWorkFile(ctx, ""), retrieveSamFileHeader(ctx), true, ctx);
-	}
-
-	/**
-	 * Retrieves the {@link SAMFileHeader} using the cache files. When adjusting the Mapper/Reducer, a manual validation
-	 * of this method is required to see if it is still up-to-date!!!
-	 * 
-	 * IMPORTANT: Be sure the exact same array order is used as defined in {@link HadoopPipelineApplication}!
-	 * 
-	 * @param context
-	 *            {@link TaskAttemptContext}
-	 * @return {@link SAMFileHeader}
-	 * @throws IllegalArgumentException
-	 * @throws IOException
-	 */
-	private SAMFileHeader retrieveSamFileHeader(TaskAttemptContext context) throws IllegalArgumentException, IOException
-	{
-		DistributedCacheHandler cacheHandler = new DistributedCacheHandler(context);
-
-		SAMFileHeader samFileHeader = new SAMFileHeader();
-
-		// Adds @SQ tags data to the SAMFileHeader.
-		String alignmentReferenceDictFile = cacheHandler.getReferenceDictFile();
-		SAMSequenceDictionary seqDict = new HadoopRefSeqDictReader().read(alignmentReferenceDictFile);
-		samFileHeader.setSequenceDictionary(seqDict);
-
-		// Retrieves the tools data stored in the tools archive info.xml file.
-		String toolsArchiveInfoXml = cacheHandler.getInfoXmlFileFromToolsArchive();
-		Map<String, SAMProgramRecord> tools = new HadoopToolsXmlReader().read(toolsArchiveInfoXml);
-
-		// Add the @PG tags for the tools within the tools archive that were used (define manually!!!).
-		samFileHeader.addProgramRecord(tools.get("bwa"));
-
-		// Retrieves the samples stored in the samples information file and adds them as SAMReadGroupRecords (@RG tags).
-		String samplesInfoFile = cacheHandler.getSamplesInfoFile();
-		List<Sample> samples = new HadoopSamplesInfoFileReader().read(samplesInfoFile);
-		for (Sample sample : samples)
-		{
-			samFileHeader.addReadGroup(sample.getAsReadGroupRecord());
-		}
-
-		// Returns the completed SAMFileHeader.
-		return samFileHeader;
+		return new KeyIgnoringBAMRecordWriter<K>(getDefaultWorkFile(ctx, ""),
+				SamFileHeaderGenerator.retrieveSamFileHeader(ctx), true, ctx);
 	}
 }
